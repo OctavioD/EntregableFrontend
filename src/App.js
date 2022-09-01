e71a53ffb393779bf36583fec0ffd552ece3ee8f
OctavioD <octavio.dogil@gmail.com>
2022-09-01 16:13:13 -0300

    COMMIT FINAL

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@
 // ESTADO: App debe manejar en su estado un número para contabilizar el total de elementos comprados.
 // MÉTODOS: App debe tener un método para aumentar este número y que pueda ser ejecutado por su nieto Item.
 // PROPS: App deberá pasar por props lo necesario a sus componenetes internos.
+import Cabecera from "./components/Cabecera";
+import Listado from "./components/Listado";
+import React, {useState} from 'react';
+
 
 function App() {
 
+  let [cantidadCompras, setCantidadCompras] = useState(0);
+  const aumentarCompras = () => {
+    setCantidadCompras(cantidadCompras + 1);
+  }
+
   return (
     <div className="App">
-      <Cabecera />
-      <Listado />
+      {/*console.log('hasta acá todo bien')*/}
+      <Cabecera cantidad={cantidadCompras}/>
+      <Listado metodoStock={aumentarCompras}/>
     </div>
   );
+  
 }
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -8,12 +8,39 @@
 //    p
 //    h5 > span    (este span debe mostrar la cantidad si es mayor a 0 "agotado" si llega a 0)
 //    button       (este boton debe permitir comprar, pero si la cantidad es menor a 0 debe estar deshabilitado y decir "Sin stock")
+import React, {useState} from 'react';
 
-export default function Item() {
+
+export default function Item(props) {
+
+  let idItem = props.props.item.id;
+  let [cantidadStock, setCantidadStock] = useState(props.props.item.stock);
+  
+  
+  const restarStock = () => {
+    setCantidadStock(cantidadStock-1);
+  }
+
+  const cambioDeStock = () => {
+    let boton = document.getElementById(idItem.toString());
+    if(cantidadStock > 0 && cantidadStock > 1){
+      props.props.metodo.metodoStock();
+      restarStock();
+    }
+    else if(cantidadStock === 1){
+      props.props.metodo.metodoStock();
+      restarStock();
+      boton.innerText = 'SIN STOCK';
+    }
+    
+  }
 
   return (
     <div className='producto'>
-      {/* maquetar Item aquí */}
+      <h3>{props.props.item.producto.nombre}</h3>
+      <p>{props.props.item.producto.descripcion}</p>
+      <h5>En stock: <span>{cantidadStock > 0?cantidadStock:'agotado'}</span></h5>
+      <button id={idItem} onClick={cambioDeStock}>COMPRAR</button>
     </div>
   )
 }
